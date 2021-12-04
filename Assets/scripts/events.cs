42dfdacb7c2a4876a97e4e0ca53235e5754b0aed
PiotrekUnityDeveloper <85606121+PiotrekUnityDeveloper@users.noreply.github.com>
2021-12-04 09:25:31 +0100

    added a bunch of player controllers so player have more control over his movement

--- Assets/scripts/events.cs
+++ Assets/scripts/events.cs
@@ -8,7 +8,8 @@ public class events : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        updateplayersfrictionandbounciness();
+        //Physics.gravity = new Vector3(1f, -9.81f, 0f);
     }
 
     // Update is called once per frame
@@ -18,13 +19,39 @@ public class events : MonoBehaviour
     }
 
     public Slider frictionslider;
+    public Slider bounceslider;
+    public Slider gravslider;
+    public Toggle revgravitycheck;
+
     public GameObject PlayerObj;
+    public PhysicsMaterial2D physmat;
+
+    public float defaultgravity = 9.81f;
 
     public void updateplayersfrictionandbounciness()
     {
         //PlayerObj.GetComponent<PhysicsMaterial2D>().friction = frictionslider.value;
-        //PlayerObj.GetComponent<BoxCollider2D>().sharedMaterial.friction = frictionslider.value;
+        PlayerObj.GetComponent<BoxCollider2D>().sharedMaterial.bounciness = bounceslider.value;
         PlayerObj.GetComponent<Rigidbody2D>().drag = frictionslider.value;
         PlayerObj.GetComponent<Rigidbody2D>().angularDrag = frictionslider.value;
+        PlayerObj.GetComponent<Rigidbody2D>().gravityScale = gravslider.value;
+
+
+        //PlayerObj.GetComponent<Collider2D>().enabled = false;
+        //PlayerObj.GetComponent<Collider2D>().enabled = true;
+
+        PlayerObj.GetComponent<BoxCollider2D>().enabled = false;
+        PlayerObj.GetComponent<BoxCollider2D>().enabled = true;
+        PlayerObj.GetComponent<BoxCollider2D>().sharedMaterial = null;
+        PlayerObj.GetComponent<BoxCollider2D>().sharedMaterial = physmat;
+
+        if(revgravitycheck.isOn == true)
+        {
+            //Physics.gravity = new Vector3(0f, defaultgravity, 0f);
+        }
+        else
+        {
+            //Physics.gravity = new Vector3(0f, (defaultgravity * -1f), 0f);
+        }
     }
 }
