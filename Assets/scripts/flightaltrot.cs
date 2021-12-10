7746847788a821f535ea7ed34b5dfc871e122995
PiotrekUnityDeveloper <85606121+PiotrekUnityDeveloper@users.noreply.github.com>
2021-12-10 20:45:40 +0100

    flashlight fully fixed

--- Assets/scripts/flightaltrot.cs
+++ Assets/scripts/flightaltrot.cs
@@ -7,10 +7,14 @@ using UnityEngine.UI;
 public class flightaltrot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public string dir;
+    public GameObject requestedrotationobject;
 
     //public GameObject flashlight;
 
     public Rigidbody2D flashlightrigidbd;
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +24,7 @@ public class flightaltrot : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
     // Update is called once per frame
     void Update()
     {
-        
+        //transform.position = GameObject.Find("ludzik").transform.position;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -28,21 +32,45 @@ public class flightaltrot : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
         if(dir == "up")
         {
             
-            flashlightrigidbd.rotation = 0;
+            //flashlightrigidbd.rotation = 0;
+
+            var rotationVector = transform.rotation.eulerAngles;
+            rotationVector.z = 0;
+            requestedrotationobject.transform.rotation = Quaternion.Euler(rotationVector);
+
+            print("flashlight rotated up");
         }
         else if (dir == "down")
         {
-            flashlightrigidbd.rotation = 180;
+            //flashlightrigidbd.rotation = 180;
+
+            var rotationVector = transform.rotation.eulerAngles;
+            rotationVector.z = 180;
+            requestedrotationobject.transform.rotation = Quaternion.Euler(rotationVector);
+
+            print("flashlight rotated down");
         }
         else if (dir == "right")
         {
             
-            flashlightrigidbd.rotation = 270;
+            ///flashlightrigidbd.rotation = 270;
+
+            var rotationVector = transform.rotation.eulerAngles;
+            rotationVector.z = 270;
+            requestedrotationobject.transform.rotation = Quaternion.Euler(rotationVector);
+
+            print("flashlight rotated right");
         }
         else if (dir == "left")
         {
             
-            flashlightrigidbd.rotation = 90;
+            //flashlightrigidbd.rotation = 90;
+
+            var rotationVector = transform.rotation.eulerAngles;
+            rotationVector.z = 90;
+            requestedrotationobject.transform.rotation = Quaternion.Euler(rotationVector);
+
+            print("flashlight rotated left");
         }
     }
 
